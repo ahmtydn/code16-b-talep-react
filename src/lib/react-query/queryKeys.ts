061629da2b8b88eb0f1a0500d6861e2e7735a039
Hakan Can <hakancn297@gmail.com>
2024-02-23 11:35:26 +0300

    lib folder completed

---
+++ src/lib/react-query/queryKeys.ts
@@ -0,0 +1,21 @@
+export enum QUERY_KEYS {
+  
+  CREATE_USER_ACCOUNT = "createUserAccount",
+
+  
+  GET_CURRENT_USER = "getCurrentUser",
+  GET_USERS = "getUsers",
+  GET_USER_BY_ID = "getUserById",
+
+  
+  GET_POSTS = "getPosts",
+  GET_INFINITE_POSTS = "getInfinitePosts",
+  GET_RECENT_POSTS = "getRecentPosts",
+  GET_POST_BY_ID = "getPostById",
+  GET_USER_POSTS = "getUserPosts",
+  GET_FILE_PREVIEW = "getFilePreview",
+  GET_COMMENTS_BY_POST_ID = "getCommentsByPostId",
+
+  
+  SEARCH_POSTS = "getSearchPosts",
+}
